c64210209f90f50c871b19e7908114ff7a04331e
Ashok <ashok.bhattarai@rumsan.net>
2026-03-03 09:06:00 +0545

    updated

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = request.cookies.get("access_token")?.value;
   const { pathname } = request.nextUrl;
 
