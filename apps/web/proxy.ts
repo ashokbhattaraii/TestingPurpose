fd330fa36089a80802a3feaab7056c1d8eb4d3ee
Ashok <ashok.bhattarai@rumsan.net>
2026-03-03 08:39:25 +0545

    updated

--- apps/web/proxy.ts
+++ apps/web/proxy.ts
@@ -40,5 +40,5 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
 
-  matcher: ["/api/:path*", "dashboard/:path*", "requests/:path*"],
+  matcher: ["/api/:path*", "/dashboard/:path*", "/requests/:path*"],
 };
